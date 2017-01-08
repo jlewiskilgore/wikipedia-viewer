1fc6d6ad58941b265c232becc5f9834db0c38c68
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-01-08 10:40:13 -0500

    Add placeholder list boxes and make GET request to Wikipedia API with hard coded data

--- js/wikiview.js
+++ js/wikiview.js
@@ -1,3 +1,14 @@
 function searchWiki() {
-	
+	console.log("search button pressed");
+
+	$.ajax({
+	  url: 'https://en.wikipedia.org/w/api.php',
+	  data: { action: 'query', list: 'search', srsearch: 'Richard Feynman', format: 'json' },
+	  dataType: 'jsonp',
+	  success: function (x) {
+	  	console.log(x.query.search[0]);
+	    console.log('title', x.query.search[0].title);
+	    console.log('snippet', x.query.search[0].snippet);
+	  }
+	});
 }
