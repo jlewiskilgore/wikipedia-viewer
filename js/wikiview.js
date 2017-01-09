b3db994054cfc306bc9b6351b1460a21211b8c40
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-01-09 17:52:05 -0500

    Get text from search box to use in API query

--- js/wikiview.js
+++ js/wikiview.js
@@ -1,11 +1,14 @@
 function searchWiki() {
 	console.log("search button pressed");
+	var searchStr = document.getElementById('searchBox').value;
+	console.log(searchStr);
 
 	$.ajax({
 	  url: 'https://en.wikipedia.org/w/api.php',
-	  data: { action: 'query', list: 'search', srsearch: 'Richard Feynman', format: 'json' },
+	  data: { action: 'query', list: 'search', srsearch: searchStr, format: 'json' },
 	  dataType: 'jsonp',
 	  success: function (x) {
+	  	console.log(x.query.search.length);
 	  	console.log(x.query.search[0]);
 	    console.log('title', x.query.search[0].title);
 	    console.log('snippet', x.query.search[0].snippet);
