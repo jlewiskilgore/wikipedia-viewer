23c5f7a16538c69da14aa7457c35a81399acf794
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-01-09 20:16:10 -0500

    clean of log statements

--- js/wikiview.js
+++ js/wikiview.js
@@ -1,7 +1,5 @@
 function searchWiki() {
-	console.log("search button pressed");
 	var searchStr = document.getElementById('searchBox').value;
-	console.log(searchStr);
 
 	$.ajax({
 	  url: 'https://en.wikipedia.org/w/api.php',
@@ -16,9 +14,7 @@ function searchWiki() {
 	    var articleURL;
 
 	    for(var i = 0; i < x.query.search.length; i++) {
-	    	console.log(x.query.search[i]);
 	    	articleURL = "https://en.wikipedia.org/wiki/" + x.query.search[i].title.replace(" ", "_");
-	    	console.log(articleURL);
 	    	listItem = "<a href="+articleURL+" target='_blank' class='list-group-item'><h2>"+x.query.search[i].title+"</h2>"+x.query.search[i].snippet+"</a><br>";
 	    	resultsContainer.innerHTML += listItem;
 	    }
