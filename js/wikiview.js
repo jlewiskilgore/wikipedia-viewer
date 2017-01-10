4f2b4b0da5b092364a032c5100608662b195d6ca
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-01-09 19:19:35 -0500

    Put results into HTML page and create links to each result's wikipedia page

--- js/wikiview.js
+++ js/wikiview.js
@@ -8,10 +8,20 @@ function searchWiki() {
 	  data: { action: 'query', list: 'search', srsearch: searchStr, format: 'json' },
 	  dataType: 'jsonp',
 	  success: function (x) {
-	  	console.log(x.query.search.length);
-	  	console.log(x.query.search[0]);
-	    console.log('title', x.query.search[0].title);
-	    console.log('snippet', x.query.search[0].snippet);
+
+	  	//Get the element of the list and clear its contents for the new search results
+	    var resultsContainer = document.getElementById("resultsList");
+	    resultsContainer.innerHTML = "";
+	    var listItem;
+	    var articleURL;
+
+	    for(var i = 0; i < x.query.search.length; i++) {
+	    	console.log(x.query.search[i]);
+	    	articleURL = "https://en.wikipedia.org/wiki/" + x.query.search[i].title.replace(" ", "_");
+	    	console.log(articleURL);
+	    	listItem = "<a href="+articleURL+" target='_blank' class='list-group-item list-group-item-action'><h2>"+x.query.search[i].title+"</h2>"+x.query.search[i].snippet+"</a><br>";
+	    	resultsContainer.innerHTML += listItem;
+	    }
 	  }
 	});
 }
