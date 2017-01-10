4cc6f3c2bfcc965756032f5b5c67c58e6b60f05c
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-01-10 18:10:06 -0500

    Fix bug when submitting empty search string

--- js/wikiview.js
+++ js/wikiview.js
@@ -1,23 +1,25 @@
 function searchWiki() {
 	var searchStr = document.getElementById('searchBox').value;
 
-	$.ajax({
-	  url: 'https://en.wikipedia.org/w/api.php',
-	  data: { action: 'query', list: 'search', srsearch: searchStr, format: 'json' },
-	  dataType: 'jsonp',
-	  success: function (x) {
+	if(searchStr !== ""){
+		$.ajax({
+		  url: 'https://en.wikipedia.org/w/api.php',
+		  data: { action: 'query', list: 'search', srsearch: searchStr, format: 'json' },
+		  dataType: 'jsonp',
+		  success: function (x) {
 
-	  	//Get the element of the list and clear its contents for the new search results
-	    var resultsContainer = document.getElementById("resultsList");
-	    resultsContainer.innerHTML = "";
-	    var listItem;
-	    var articleURL;
+		  	//Get the element of the list and clear its contents for the new search results
+		    var resultsContainer = document.getElementById("resultsList");
+		    resultsContainer.innerHTML = "";
+		    var listItem;
+		    var articleURL;
 
-	    for(var i = 0; i < x.query.search.length; i++) {
-	    	articleURL = "https://en.wikipedia.org/wiki/" + x.query.search[i].title.replace(" ", "_");
-	    	listItem = "<a href="+articleURL+" target='_blank' class='list-group-item'><h2>"+x.query.search[i].title+"</h2>"+x.query.search[i].snippet+"</a><br>";
-	    	resultsContainer.innerHTML += listItem;
-	    }
-	  }
-	});
+		    for(var i = 0; i < x.query.search.length; i++) {
+		    	articleURL = "https://en.wikipedia.org/wiki/" + x.query.search[i].title.replace(" ", "_");
+		    	listItem = "<a href="+articleURL+" target='_blank' class='list-group-item'><h2>"+x.query.search[i].title+"</h2>"+x.query.search[i].snippet+"</a><br>";
+		    	resultsContainer.innerHTML += listItem;
+		    }
+		  }
+		});
+	}
 }
